d3c8f927b58d32f136b2a5b981063b87fd1ce910
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-07-12 18:50:28 +0530

    react-router-dom add

--- blog-front-end/src/App.js
+++ blog-front-end/src/App.js
@@ -6,17 +6,34 @@ import { BlogList } from "./components/BlogList";
 import { CreateBlog } from "./components/CreateBlog";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
    return (
-      <div className="App">
-         <NavBar />
-         {/* <Login /> */}
-         {/* <Register /> */}
-         {/* <Blog /> */}
-         {/* <BlogList /> */}
-         <CreateBlog />
-      </div>
+      <Router>
+         <div className="App">
+            <NavBar />
+            <Switch>
+               <Route exact path="/">
+                  <BlogList />
+               </Route>
+               <Route path="/home">
+                  <BlogList />
+               </Route>
+               <Route path="/login">
+                  <Login />
+               </Route>
+               <Route path="/register">
+                  <Register />
+               </Route>
+               {/* <Login /> */}
+               {/* <Register /> */}
+               {/* <Blog /> */}
+               {/* <BlogList /> */}
+               <CreateBlog />
+            </Switch>
+         </div>
+      </Router>
    );
 }
 
--- blog-front-end/src/components/Nav.js
+++ blog-front-end/src/components/Nav.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-
+import { Link } from "react-router-dom";
 export const NavBar = () => {
    return (
       <Navbar bg="dark" variant="dark">
          <div className="container">
-            <Navbar.Brand href="#home">Blogs</Navbar.Brand>
+            <Navbar.Brand href="home">Bhaskar's Blogs</Navbar.Brand>
             <Nav className="ml-auto">
-               <Nav.Link href="#home">
+               <Link className="link" to="home">
                   <AccountCircleIcon />
-               </Nav.Link>
-               <Nav.Link href="#features">Login</Nav.Link>
-               <Nav.Link href="#pricing">Logout</Nav.Link>
+               </Link>
+               <Link className="link" to="create">
+                  Create
+               </Link>
+               <Link className="link" to="login">
+                  Login
+               </Link>
+               <Link className="link" to="register">
+                  Register
+               </Link>
+               <Link className="link" to="logout">
+                  Logout
+               </Link>
             </Nav>
          </div>
       </Navbar>
