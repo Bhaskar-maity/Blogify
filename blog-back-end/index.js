316d03b5293c74b4fae9a9de9ee7d3bd7f613a61
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-07-13 12:50:43 +0530

    multer file upload

--- blog-back-end/index.js
+++ blog-back-end/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const multer = require("multer");
+
 dotenv.config();
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -15,6 +17,20 @@ mongoose
    .then(console.log("mongodb connected"))
    .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+   destination: (req, file, cb) => {
+      cb(null, "imagesUploads");
+   },
+   filename: (req, file, cb) => {
+      cb(null, req.body.name);
+   },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("file"), (req, res) => {
+   res.status(200).json("File uploaded");
+});
 app.use("/auth", authRoute);
 app.use("/post", postRoute);
 
