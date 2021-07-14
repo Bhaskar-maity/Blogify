5f6b674db7d1f0a002c5c0ddc4b4a4d75f510a52
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-07-14 22:54:06 +0530

    bycript add, modify some routes

--- blog-back-end/index.js
+++ blog-back-end/index.js
@@ -3,16 +3,22 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
    .connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
    })
    .then(console.log("mongodb connected"))
    .catch((err) => console.log(err));
@@ -28,11 +34,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post("/api/upload", upload.single("file"), (req, res) => {
    res.status(200).json("File uploaded");
 });
-app.use("/auth", authRoute);
-app.use("/post", postRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
    console.log("app running");
