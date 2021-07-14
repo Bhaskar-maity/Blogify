38ac38012c2911da8019b9192426f6225cfedf33
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-07-14 21:26:33 +0530

    new api add

--- blog-back-end/routes/auth.js
+++ blog-back-end/routes/auth.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
+//REGISTER
 router.post("/register", async (req, res) => {
    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPass = await bcrypt.hash(req.body.password, salt);
       const newUser = new User({
+         username: req.body.username,
          email: req.body.email,
-         password: req.body.password,
+         password: hashedPass,
       });
+
       const user = await newUser.save();
       res.status(200).json(user);
    } catch (err) {
@@ -14,18 +20,19 @@ router.post("/register", async (req, res) => {
    }
 });
 
+//LOGIN
 router.post("/login", async (req, res) => {
    try {
-      const user = await User.findOne({
-         email: req.body.email,
-      });
-      !user && res.status(400).json("wrong credentials");
-      const validate = req.body.password === user.password;
-      !validate && res.status(400).json("wrong credentials");
+      const user = await User.findOne({ username: req.body.username });
+      !user && res.status(400).json("Wrong credentials!");
+
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      !validated && res.status(400).json("Wrong credentials!");
+
       const { password, ...others } = user._doc;
       res.status(200).json(others);
    } catch (err) {
-      console.log(err);
+      res.status(500).json(err);
    }
 });
 
