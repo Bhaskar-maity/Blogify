27c2410d3445b9c043a2f5bc3dba6173ee91fe2c
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-07-15 12:34:51 +0530

    context add

--- blog-front-end/src/App.js
+++ blog-front-end/src/App.js
@@ -1,39 +1,31 @@
-import { NavBar } from "./components/Nav";
-import { Login } from "./components/Login";
-import { Register } from "./components/Register";
-import { Blog } from "./components/Blog";
-import { BlogList } from "./components/BlogList";
-import { CreateBlog } from "./components/CreateBlog";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/home/Home";
+import TopBar from "./components/topbar/TopBar";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
+   const { user } = useContext(Context);
    return (
       <Router>
-         <div className="App">
-            <NavBar />
-            <Switch>
-               <Route exact path="/">
-                  <BlogList />
-               </Route>
-               <Route path="/home">
-                  <BlogList />
-               </Route>
-               <Route path="/login">
-                  <Login />
-               </Route>
-               <Route path="/register">
-                  <Register />
-               </Route>
-               <Route path="/create">
-                  <CreateBlog />
-               </Route>
-               <Route path="/post/:pid">
-                  <Blog />
-               </Route>
-            </Switch>
-         </div>
+         <TopBar />
+         <Switch>
+            <Route exact path="/">
+               <Home />
+            </Route>
+            <Route path="/register">{user ? <Home /> : <Register />}</Route>
+            <Route path="/login">{user ? <Home /> : <Login />}</Route>
+            <Route path="/write">{user ? <Write /> : <Register />}</Route>
+            <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+            <Route path="/post/:postId">
+               <Single />
+            </Route>
+         </Switch>
       </Router>
    );
 }
